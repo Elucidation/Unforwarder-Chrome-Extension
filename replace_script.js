2a67c89053bcac4594227b304a53a83e4654fc9f
Sameer Ansari <elucidation@gmail.com>
2011-11-03 01:56:56 -0400

    New icon, autoupdate was broke from before, button click works

--- replace_script.js
+++ replace_script.js
@@ -1,3 +1,11 @@
+// Script run on a google search result page, replacing all google-forwarded result links with original links in place.
+// Example starting google-forwarded url :
+//              http://www.google.com/url?sa=t&rct=j&q=fear%20not%20the%20valient&source=web&cd=1&ved=0CCAQFjAA&url=http%3A%2F%2Fen.wikiquote.org%2Fwiki%2FCourage&ei=QiiyTu2QGZHAtgfB762cAg&usg=AFQjCNHOaIV8vNJ1fxGxdNmYkl3v9jpumw&sig2=IbeYKkY9u-lfRGUA38gRtw
+// Fixed ending url: 
+//              http://en.wikiquote.org/wiki/Courage
+// Author : Sameer Ansari
+
+// Regex for getting original url out of a google-forwarded url
 var reg = new RegExp("url=([^&]+)");
 // For each search term section (which has first child as the a href link)
 for (i=0; i < document.getElementsByClassName("r").length; i++) {
