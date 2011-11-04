4640c1ac447db65dd8956c348d830d1fe5724a7e
Elucidation <elucidation@gmail.com>
2011-11-03 20:23:51 -0400

    Allan's Fix to replace script based off online source

--- replace_script.js
+++ replace_script.js
@@ -1,20 +1,54 @@
-// Script run on a google search result page, replacing all google-forwarded result links with original links in place.
-// Example starting google-forwarded url :
-//              http://www.google.com/url?sa=t&rct=j&q=fear%20not%20the%20valient&source=web&cd=1&ved=0CCAQFjAA&url=http%3A%2F%2Fen.wikiquote.org%2Fwiki%2FCourage&ei=QiiyTu2QGZHAtgfB762cAg&usg=AFQjCNHOaIV8vNJ1fxGxdNmYkl3v9jpumw&sig2=IbeYKkY9u-lfRGUA38gRtw
-// Fixed ending url: 
-//              http://en.wikiquote.org/wiki/Courage
-// https://github.com/Elucidation/Unforwarder-Chrome-Extension
-// Author : Sameer Ansari
+var linkPatterns =
+[
+    /^(?:http(?:s)?:\/\/(?:\w+\.)*google\.\w{2,3})?\/url\?(?:.+&)?url=([^&]*)(?:&|$)/,
+    /^(?:http(?:s)?:\/\/(?:\w+\.)*google\.\w{2,3})?\/url\?(?:.+&)?q=([^&]*)(?:&|$)/
+];
+var strippedEvents = [ "onclick", "onmousedown", "onmouseup", "onkeypress", "onkeydown", "onkeyup" ];
 
-// Regex for getting original url out of a google-forwarded url
-var reg = new RegExp("url=([^&]+)");
-// For each search term section (which has first child as the a href link)
-for (i=0; i < document.getElementsByClassName("r").length; i++) {
-	// The link is the first child href=
-	x = document.getElementsByClassName("r")[i].firstChild.href;
-	// If the regex match shows a google-encoded url
-	if (x.match(reg) != null) {
-      // Replace original url w/ fixed url
-		document.getElementsByClassName("r")[i].firstChild.href = decodeURIComponent(x.match(reg)[1]);
-	}
+function processNode(node)
+{
+    if (node.nodeType != 1 || node.hasAttribute("linkChecked"))
+        return;
+
+    if (node.tagName.toLowerCase() == "a")
+        processLink(node);
+
+    for (i=0;i<node.getElementsByTagName("a").length;i++) {
+    	processLink(node.getElementsByTagName("a")[i]);
+    }
+
+    node.setAttribute("linkChecked", "yes");
 }
+
+function processLink(node)
+{
+    var url = node.href, cleanUrl = null;
+
+    var matches;
+    for (var i in linkPatterns)
+        if (matches = linkPatterns[i].exec(url))
+            break;
+
+    if (matches)
+    {
+        cleanUrl = decodeURIComponent(matches[1]);
+        node.href = cleanUrl;
+    }
+    else if (!/\s(rwt|clk)\s*\(/.test(node.onmousedown))
+        return;
+
+    for (var i in strippedEvents)
+        node[strippedEvents[i]] = null;
+}
+
+function deploy()
+{
+    document.addEventListener("DOMNodeInserted", function(e)
+    {
+        processNode(e.target);
+    }, false);
+
+    processNode(document.body);
+}
+
+deploy();
